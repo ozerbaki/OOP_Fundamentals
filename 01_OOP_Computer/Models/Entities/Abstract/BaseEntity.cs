71eaa9842be765eaadf799566f3d2d0c0e79a81f
Abdülbaki Özer <abdulbaki.ozer@bilgeadamboost.com>
2022-04-17 02:24:00 +0300

    First of all, I marked our BaseClass as Abstratc because it will contain the basic properties of the Computer Object and we will use these properties in all subclasses. I started describing the properties of the Computer Object

---
+++ 01_OOP_Computer/Models/Entities/Abstract/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_OOP_Computer.Models.Entities.Abstract
+{
+    public abstract class BaseEntity<T>
+    {
+        public abstract T Id { get; set; }
+    }
+}
